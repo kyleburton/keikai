408b0f623b24b9fdccc31b0f155dcdffb4532bba
Kyle Burton <kyle.burton@gmail.com>
2012-04-24 14:22:38 -0400

    use local classpath, and release version of swank-clojure

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
 (defproject keikai "0.1.0-SNAPSHOT"
   :description "Monitoring and metrics framework for distributed systems."
   :url "http://github.com/codeslinger/keikai"
+  :local-repo-classpath true
   :dependencies [
     [clojure "1.3.0"]
     [io.netty/netty "3.3.1.Final"]
     [org.clojure/tools.logging "0.2.3"]
   ]
   :dev-dependencies [
-    [swank-clojure "1.4.0-SNAPSHOT"]
+    [swank-clojure "1.4.2"]
   ]
   :main keikai.cli
 )
